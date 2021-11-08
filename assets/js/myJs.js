2ec8b5aefc0b8f4f816a9a7e337875654ef79d0a
zaenal15 <77434826+zaenal15@users.noreply.github.com>
2021-11-08 09:10:02 +0700

    Dashboard Speaker (08-11-2021)

--- assets/js/myJs.js
+++ assets/js/myJs.js
@@ -138,6 +138,22 @@ $('#closeFilter').on('click', function(){
   $('#closeFilter').hide();
 });
 
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+      $('#imagePreview').hide();
+      $('#imagePreview').fadeIn(650);
+    }
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$("#imageUpload").change(function() {
+    readURL(this);
+});
+
 //dasboard filter
 $(document).ready(function(){
   $('a#filter-a').click(function(){
